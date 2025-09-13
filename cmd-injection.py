3ea1af04fdeb18f10e55247a6649c3a28b1e6604
EL-FAHIM  Lahcen <fahim.etu@gmail.com>
2025-09-13 23:18:07 +0200

    Update cmd-injection.py

--- cmd-injection.py
+++ cmd-injection.py
@@ -1,10 +1,2 @@
 import os
-import subprocess
-
-user_input = input("Enter a command: ")
-
-# Command injection possible
-os.system(user_input)
-
-# Ou avec subprocess
-subprocess.run(user_input, shell=True)
+os.system(input("Commande: "))  # Cette ligne sera détectée
